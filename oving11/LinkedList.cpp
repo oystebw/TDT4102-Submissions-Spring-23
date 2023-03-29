003b8e3c5d8d08b9b6313ce551f65d018a8fd680
Øystein Weibell <oystebw@stud.ntnu.no>
2023-03-29 09:15:59 +0200

    Added prototypes assign. 11

---
+++ oving11/LinkedList.cpp
@@ -0,0 +1,30 @@
+#include "LinkedList.h"
+
+namespace LinkedList{
+
+std::ostream& operator<<(std::ostream& os, const Node& node){
+    os << node.value << '\n';
+    return os;
+}
+
+Node* LinkedList::insert(Node* pos, const std::string& value){
+
+}
+
+Node* LinkedList::remove(Node* pos){
+
+}
+
+Node* LinkedList::find(const std::string& value){
+
+}
+
+void LinkedList::remove(const std::string& value){
+
+}
+
+std::ostream& operator<<(std::ostream& os, const LinkedList& list){
+    
+}
+
+}
